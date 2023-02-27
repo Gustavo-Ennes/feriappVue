14dd6d7231db7eb2826b51968c691a06cada35dd
Gustavo Ennes <gustavo@ennes.dev>
2023-02-26 21:50:44 -0300

    Route guards

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,9 +1,17 @@
+import { store } from "@/store/store";
 import { createRouter, createWebHistory } from "vue-router";
 import { routes } from "../routes/routes";
 
 const router = createRouter({
   history: createWebHistory("/"),
   routes,
+});
+
+router.beforeEach((to, from) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    return !!store.state.user ? true : router.push("/login");
+  }
+  return true;
 });
 
 export { router };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,20 @@
+import { router } from "@/router/router";
+import { store } from "@/store/store";
 import type { RouteRecordRaw } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
-  { path: "/login", component: () => import("./login/Login.vue") },
-  // {
-  //   path: "/pokemon/:search/",
-  //   component: () => import("./Pokemon/PokemonPage.vue"),
-  // },
+  {
+    path: "/login",
+    component: () => import("./login/Login.vue"),
+    beforeEnter() {
+      return store.state.user ? router.push("/home") : true;
+    },
+  },
+  {
+    path: "/home",
+    component: () => import("./home/Home.vue"),
+    meta: { requiresAuth: true },
+  },
 ];
 
 export { routes };
