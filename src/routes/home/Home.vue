46ea44d9cfe68624acd548c2be9f0a6c77a2c9d0
Gustavo Ennes <gustavo@ennes.dev>
2023-02-26 21:50:21 -0300

    Home base

---
+++ src/routes/home/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="row justify-content-around align-items-stretch mx-0 mt-4">
+    <div class="col-sm-12 col-md-6 col-lg-4">
+      <MainButton label="Férias" action="" />
+    </div>
+    <div class="col-sm-12 col-md-6 col-lg-4">
+      <MainButton label="Licença-prêmio" action="" />
+    </div>
+    <div class="col-sm-12 col-md-6 col-lg-4">
+      <MainButton label="Abono" action="" />
+    </div>
+    <div class="col-sm-12 col-md-6 col-lg-4">
+      <MainButton label="Justificativas" action="" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import MainButton from "./components/MainButton.vue";
+
+export default {
+  name: "Home",
+  components: { MainButton },
+};
+</script>
+
+<style lang="scss">
+.row {
+  max-width: 100vw;
+  display: flex;
+}
+</style>
---
+++ src/routes/home/components/MainButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="my-2 button d-grid gap-2 w-100">
+    <button class="btn btn-primary text-center" @click="action">
+      {{ label }}
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "MainButton",
+  props: ["label", "action"],
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+  width: 200px;
+  text-align: center;
+}
+.btn {
+  width: 100%;
+}
+</style>
