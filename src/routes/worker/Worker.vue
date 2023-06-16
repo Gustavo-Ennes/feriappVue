e81c28e9ac5fdda26bed8bca809a409ff096a228
Gustavo-Ennes <gustavo@ennes.dev>
2023-06-16 10:07:33 -0300

    Worker edit modal in Worker page

--- src/routes/worker/Worker.vue
+++ src/routes/worker/Worker.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="container text-center my-5">
     <div class="row justify-content-start align-items-center g-2">
-      <h1 class="col-12 text-primary">
-        {{ capitalizeName(worker?.name ?? "") }}
+      <div class="col-3" />
+      <h1 class="col-6 text-primary">
+        {{ capitalizeName(worker?.name ?? "nome") }}
       </h1>
+      <div class="col-3">
+        <button class="btn btn-primary" @click="handleOpenModal">
+          <i class="text-warning fa-solid fa-pen" /> Editar
+        </button>
+      </div>
       <h6 class="col-12 mb-5 text-info">Cargo: {{ worker?.role }}</h6>
       <div class="col-12 col-sm-6">
         <i class="fa-solid fa-address-card text-primary" />
@@ -25,44 +31,62 @@
             data-bs-toggle="tooltip"
             data-bs-placement="top"
             :title="`Visitar o departamento de ${capitalizeName(
-              worker?.department.name ?? ''
+              worker?.department.name ?? 'nome'
             )}`"
             >Departamento:
-            {{ capitalizeName(worker?.department.name ?? "") }}</router-link
+            {{ capitalizeName(worker?.department.name ?? "nome") }}</router-link
           >
         </p>
+      </div>
+      <div class="col-12 col-sm-6">
+        <i class="fa-solid fa-dollar text-primary" />
+        <p>Justificativa de H.E.: {{ computedJustification }}</p>
       </div>
       <div class="col-12">
         <div class="mt-5"><WorkerVacations :vacations="workerVacations" /></div>
       </div>
     </div>
   </div>
+  <WorkerModal
+    type="edit"
+    :modal="modal"
+    :worker="worker"
+    @form-submitted="getWorkerAndVacations"
+  />
 </template>
 
 <script lang="ts">
 import { format } from "date-fns";
+import { Modal } from "bootstrap";
 
 import WorkerVacations from "./components/workerVacations/WorkerVacations.vue";
+import DrasticConfirmationModal from "../../components/DrasticConfirmationModal.vue";
 import type { WorkerDataType } from "./types";
 import { workerAndVacations } from "./fetch";
-import { getVacations } from "./utils";
+import WorkerModal from "../../components/WorkerModal/WorkerModal.vue";
 import { capitalizeName } from "@/routes/utils";
 
 export default {
   name: "Worker",
   props: ["_id"],
+  emits: ["selectWorker", "openModal"],
   data(): WorkerDataType {
     return {
       worker: undefined,
       workerVacations: undefined,
+      modal: undefined,
     };
   },
   async beforeMount(): Promise<void> {
-    const { data } = await workerAndVacations({ _id: this._id });
-    this.worker = data.worker;
-    this.workerVacations = data.vacations;
+    await this.getWorkerAndVacations();
+  },
+  mounted() {
+    this.instantiateModal();
   },
   computed: {
+    computedJustification() {
+      return this.worker?.justification;
+    },
     computedAdmissionDate(): string {
       return this.worker?.admissionDate
         ? format(new Date(this.worker.admissionDate), "dd/MM/yyyy")
@@ -71,8 +95,19 @@ export default {
   },
   methods: {
     capitalizeName,
-    getVacations,
+    handleOpenModal() {
+      this.modal?.show();
+    },
+    async getWorkerAndVacations() {
+      const { data } = await workerAndVacations({ _id: this._id });
+      this.worker = data.worker;
+      this.workerVacations = data.vacations;
+    },
+    instantiateModal(): void {
+      const modalHTML = document.getElementById(`worker-modal`);
+      this.modal = new Modal(modalHTML as HTMLElement, {});
+    },
   },
-  components: { WorkerVacations },
+  components: { WorkerVacations, WorkerModal, DrasticConfirmationModal },
 };
 </script>
--- src/routes/worker/types.ts
+++ src/routes/worker/types.ts
@@ -1,6 +1,11 @@
 import type { Worker } from "../workers/types";
 import type { Vacation } from "../vacation/types";
+import type { Modal } from "bootstrap";
 
-type WorkerDataType = { worker?: Worker; workerVacations?: Vacation[] };
+type WorkerDataType = {
+  worker?: Worker;
+  workerVacations?: Vacation[];
+  modal?: Modal;
+};
 
 export type { WorkerDataType };
